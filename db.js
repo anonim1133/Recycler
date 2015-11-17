4076a758dd05a1171bff046bc16976778dbb018c
Krzysztof Oleśków <KOleskow@si-consulting.pl>
2015-11-17 10:05:56 +0100

    If file is new checking for similar images

--- db.js
+++ db.js
@@ -1,39 +1,29 @@
 var pg = require('pg');
 
-module.exports.query = function(query){
-	pg.connect(global.config.PG_CONNECTION_STRING, function(err, client, done) {
-		if (err) {
-			return console.error('error fetching client from pool', err);
-		}
-		client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-		
-		done();
-		
-		if (err) {
-			return console.error('error running query', err);
-		}
-		
-		return result;
-		});
-
-	});
-}
-
 module.exports.check = function(name, where, hash, ret){
 	pg.connect(global.config.PG_CONNECTION_STRING, function(err, client, done) {
 		if (err) {
 			return console.error('error fetching client from pool', err);
 		}
 		
-		client.query('INSERT INTO image_duplicates("where", "original", "hash") VALUES ($1, $2, $3::bit(1024))', [where, name, hash], function(err, result) {
+		client.query('INSERT INTO image_duplicates("where", "original", "hash") VALUES ($1, $2, $3::bit(1024))', [where, name, hash], function(err) {
 			if(err != null){
 				console.log("DB Error: \n" + err);
 				if(err.code === '23505')
-					ret({status: "duplicate"})
+					ret({status: "duplicate"})// Get that duplicate
 				else
 					ret({status: "error"});
 			}else{
-				ret({status: "new"})
+				//Returning an array of similar results
+				console.log('[DEBUG] Searching for similar images');
+				client.query('SELECT original, "where", similarity($1, hash) as similar FROM image_duplicates WHERE hash != $1 ORDER BY similarity($1, hash) DESC LIMIT 3;', [hash], function(err, result) {
+					if(err != null){
+						console.log("DB Error: \n" + err);
+						ret({status: "error"});
+					}else{
+						ret({status: "new", result: result.rows});
+					}
+				});
 			}
 			
 			done();
